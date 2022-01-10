1b8c5c838458fc6e82c8a80678dd1a0a5ea514f4
Joey A <joeyazizoff@gmail.com>
2022-01-10 13:35:13 -0500

    Modal Project
    Basic project opening/closing a modal in JS

---
+++ Modal/script.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btn = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+const btnCloseModal = document.querySelector('.close-modal');
+
+const openModal = function() {
+    modal.classList.remove('hidden'); //No need to use the .
+    overlay.classList.remove('hidden');
+};
+
+const closeModal = function() {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+    btnsOpenModal[i].addEventListener('click', openModal);
+}
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
